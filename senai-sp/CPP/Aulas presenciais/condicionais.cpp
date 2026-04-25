e6236fb6ee96a241b2c70e993ce1bb9e5e9ac766
Iann Gabriel <iann.domingos@gmail.com>
2026-04-25 10:21:45 -0300

    atualizando diretorio

--- senai-sp/CPP/Aulas presenciais/condicionais.cpp
+++ senai-sp/CPP/Aulas presenciais/condicionais.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 int main() {
+    int idade = 18;
+    //console out é o cout e cin é console in
+    cin >> idade;
+    cout << idade;
 
+    if(idade >= 18){
+        cout<< "Você é maior de idade!";
+    }
 
     return 0;
 }
